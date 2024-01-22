ba3939960a92f72079cc997eec6ca4971ee230eb
ToddM <rtoddm@gmail.com>
2024-01-22 01:12:09 -0600

    JS Completed -- Search Function Added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 //Profile "overview" class
 const profileOverview = document.querySelector(".overview");
+//GitHub Username
 const userName = "rtoddm";
+const repoList = document.querySelector(".repo-list");
+
+//Selects <section> where all repo information appears -- includes an unordered list
+const repos = document.querySelector(".repos");
+//Selects the <section> where the inddividual repo data will appear
+const repoData = document.querySelector(".repo-data");
+
+//Selects the "Back to Repo Gallery Button" -- "view-repos" class
+const viewReposButton = document.querySelector(".view-repos");
+//Selects Input Element
+const filterInput = document.querySelector(".filter-repos");
 
 //Github Profile Info - Async Function
 const githubProfile = async function () {
@@ -13,6 +25,7 @@ const githubProfile = async function () {
 
 githubProfile();
 
+// Function to Display User Info from JSON Response
 const displayUserData = function (response) {
   const div = document.createElement("div");
   div.classList.add("user-info");
@@ -29,4 +42,97 @@ const displayUserData = function (response) {
   `;
 
   profileOverview.append(div);
+  fetchRepos();
 };
+
+//Function to Fetch Repos - Async Function
+const fetchRepos = async function () {
+  const repos = await fetch(
+    `https://api.github.com/users/${userName}/repos?sort=updated&per_page=100`
+  );
+  //console.log(repos);
+  const usersRepos = await repos.json();
+  console.log(usersRepos);
+
+  displayRepos(usersRepos);
+};
+
+//Function to Display Info about Repos
+const displayRepos = function (usersRepos) {
+  for (let elem of usersRepos) {
+    const li = document.createElement("li");
+    li.classList.add("repo");
+    li.innerHTML = `<h3>${elem.name}</h3>`;
+    repoList.append(li);
+  }
+  filterInput.classList.remove("hide");
+};
+
+//EventListener to Grab the Name of the Repo That's Clicked
+repoList.addEventListener("click", function (e) {
+  if (e.target.matches("h3")) {
+    const repoName = e.target.innerText;
+    specificRepo(repoName);
+  }
+});
+
+//Function to Grab Specific Repo Info
+const specificRepo = async function (repoName) {
+  const fetchRepoInfo = await fetch(
+    `https://api.github.com/repos/${userName}/${repoName}`
+  );
+  const repoInfo = await fetchRepoInfo.json();
+
+  //Grab Languages
+  const fetchLanguages = await fetch(repoInfo.languages_url);
+  const languageData = await fetchLanguages.json();
+  console.log(languageData);
+
+  const languages = [];
+  for (let elem in languageData) {
+    languages.push(elem);
+  }
+
+  //   console.log(languages);
+
+  displayInfo(repoInfo, languages);
+};
+
+//Function to Display Specific Repo Info
+const displayInfo = function (repoInfo, languages) {
+  repoData.innerHTML = "";
+  const newDiv = document.createElement("div");
+  newDiv.innerHTML = `<h3>Name: ${repoInfo.name}</h3>
+  <p>Description: ${repoInfo.description}</p>
+  <p>Default Branch: ${repoInfo.default_branch}</p>
+  <p>Languages: ${languages.join(", ")}</p>
+  <a class="visit" href="${
+    repoInfo.html_url
+  }" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>`;
+  repoData.append(newDiv);
+  repoData.classList.remove("hide");
+  repos.classList.add("hide");
+  viewReposButton.classList.remove("hide");
+};
+
+viewReposButton.addEventListener("click", function () {
+  repos.classList.remove("hide");
+  repoData.classList.add("hide");
+  viewReposButton.classList.add("hide");
+});
+
+filterInput.addEventListener("input", function (e) {
+  const searchTextValue = e.target.value;
+  const repos = document.querySelectorAll(".repo");
+  const lowercaseSearch = searchTextValue.toLowerCase();
+
+  for (let elem of repos) {
+    const lowerCaseInnerText = elem.innerText.toLowerCase();
+    if (lowerCaseInnerText.includes(lowercaseSearch)) {
+      elem.classList.remove("hide");
+    } else {
+      elem.classList.add("hide");
+    }
+    console.log(typeof repos);
+  }
+});
